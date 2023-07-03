47ddf03f12dec8b7ea405cc3454661f51e229ef8
YuJin Kwon <tokyj515@naver.com>
2023-07-04 04:41:17 +0900

    initialize

---
+++ src/main/java/com/example/neoul/NeoulApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoulApplication.class, args);
+	}
+
+}
