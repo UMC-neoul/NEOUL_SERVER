aa72701ffee1c37b5d1e5b252da9bdf65787d826
Son <rooneei@naver.com>
2023-07-09 12:46:15 +0900

    [fix] test 추가 및 region 추가

---
+++ src/main/java/com/example/neoul/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.neoul.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        log.info("test api 수신");
+        return new ResponseEntity("test ok",HttpStatus.OK);
+    }
+}
