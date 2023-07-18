a43535646361a4604751d5ef51050978bae4ab73
YuJin Kwon <tokyj515@naver.com>
2023-07-18 17:44:52 +0900

    [Feat] 카카오 소셜로그인 수정 #1

--- src/main/java/com/example/neoul/controller/AuthController.java
+++ src/main/java/com/example/neoul/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.neoul.controller;
 import com.example.neoul.dto.TokenRes;
 import com.example.neoul.dto.UserReq;
 import com.example.neoul.global.entity.BaseEntity;
+import com.example.neoul.global.exception.BadRequestException;
 import com.example.neoul.service.AuthService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +24,7 @@ public class AuthController {
     @ApiOperation(value = "인가코드 캐치를 위한 api, 사용x, 백엔드 터미널로 반환중",
             notes = "원래는 프론트엔드가 첫 회원가입 링크로 들어가서 code를 받고, 그 받은 코드로 이 api에 접근해서" +
                     "카카오의 access_token을 반환 후" +
-                    "access_token을 아래의 api에 넣어서 우리 사이트의 로그인하고 그 결과를 얻음")
+                    "access_token을 /kakao/login의 요청에 넣어서 우리 사이트의 로그인하고 그 결과를 얻음")
     @GetMapping("/kakao")
     public String getAccessTokenKakao(@RequestParam String code) {
         String accessToken=authService.getKakaoAccessToken(code);
@@ -39,7 +40,7 @@ public class AuthController {
         TokenRes tokenRes = authService.createAndLoginKakaoUser(socialReq);
 
         if(tokenRes == null)
-            return null;
+            throw new BadRequestException("사용자 정보가 없습니다");
         return tokenRes;
     }
 
