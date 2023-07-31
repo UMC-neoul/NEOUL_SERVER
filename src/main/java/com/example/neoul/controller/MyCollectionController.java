59171acbdb2a5ee81115d4edbacedb679fe444a4
YuJin Kwon <tokyj515@naver.com>
2023-07-31 20:23:22 +0900

    Feat: 마이콜렉션 컨트롤러 생성

---
+++ src/main/java/com/example/neoul/controller/MyCollectionController.java
@@ -0,0 +1,17 @@
+package com.example.neoul.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags={"04.myCollection"})
+@RequestMapping("/my-collection")
+public class MyCollectionController {
+
+
+
+
+}
