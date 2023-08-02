e4d7543b4a1bc239d07d3090ed4381a6399be730
Son <rooneei@naver.com>
2023-08-02 17:05:06 +0900

    [feat] 브랜드 찜,찜 취소,찜한 브랜드 조회

--- src/main/java/com/example/neoul/service/BrandService.java
+++ src/main/java/com/example/neoul/service/BrandService.java
@@ -2,9 +2,16 @@ package com.example.neoul.service;
 
 import com.example.neoul.dto.brand.BrandRes;
 import com.example.neoul.entity.brand.Brand;
+import com.example.neoul.entity.user.User;
+import com.example.neoul.global.exception.NotFoundException;
 import com.example.neoul.repository.BrandRepository;
 import com.example.neoul.repository.ProductRepository;
+import com.example.neoul.repository.UserLikedBrandRepository;
+import com.example.neoul.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.BeanUtils;
 
@@ -14,9 +21,12 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BrandService {
     private final BrandRepository brandRepository;
     private final ProductRepository productRepository;
+    private final UserService userService;
+
 
     public List<BrandRes.BrandListRes> getBrandlist(){
         List<Brand> brandList= brandRepository.findAll();
@@ -40,6 +50,17 @@ public class BrandService {
         return responseList;
     }
 
+    public List<BrandRes.ProductListRes> productList(Brand brand){
+        return productRepository.findAllByBrand(brand).stream()
+                .map(source -> {
+                    BrandRes.ProductListRes target = new BrandRes.ProductListRes(); // 새로운 Product 객체 생성 (속성 복사를 위한 대상 객체)
+                    BeanUtils.copyProperties(source, target); // 속성 복사
+                    target.setProductId(source.getId());
+                    return target;
+                })
+                .collect(Collectors.toList());
+    }
+
     public BrandRes.BrandInfoRes getBrandInfo(Long brandId){
         Brand brand = brandRepository.findById(brandId).get(); // 실패시 exception 발생
         BrandRes.BrandInfoRes brandInfo = BrandRes.BrandInfoRes.builder()
@@ -59,13 +80,57 @@ public class BrandService {
 
     }
 
-    public List<BrandRes.ProductListRes> productList(Brand brand){
-        return productRepository.findAllByBrand(brand).stream()
-                .map(source -> {
-                    BrandRes.ProductListRes target = new BrandRes.ProductListRes(); // 새로운 Product 객체 생성 (속성 복사를 위한 대상 객체)
-                    BeanUtils.copyProperties(source, target); // 속성 복사
-                    return target;
+    public BrandRes.makeLikedBrandRes makeLikedBrand(Long brandId){
+        User user = userService.findNowLoginUser();
+        Brand likedBrand = brandRepository.findById(brandId)
+                .orElseThrow(() -> new NotFoundException("브랜드가 존재하지 않습니다"));
+        likedBrand.setLikedUser(user);
+        brandRepository.save(likedBrand);
+        log.info("브랜드 찜 완료");
+
+        BrandRes.makeLikedBrandRes likedBrandRes= BrandRes.makeLikedBrandRes.builder()
+                .likedBrandId(brandId)
+                .likedBrandName(likedBrand.getName())
+                .build();
+
+        return likedBrandRes;
+    }
+
+    public BrandRes.deleteLikedBrandRes deleteLikedBrand(Long brandId){
+        Brand dislikedBrand = brandRepository.findById(brandId)
+                .orElseThrow(() -> new NotFoundException("브랜드가 존재하지 않습니다"));
+        dislikedBrand.setLikedUser(null);
+        brandRepository.save(dislikedBrand);
+        log.info("브랜드 찜 취소 완료");
+
+        BrandRes.deleteLikedBrandRes dislikedBrandRes= BrandRes.deleteLikedBrandRes.builder()
+                .dislikedBrandId(brandId)
+                .dislikedBrandName(dislikedBrand.getName())
+                .build();
+
+        return dislikedBrandRes;
+    }
+
+
+    public BrandRes.getLikedBrandRes getLikedBrand(){
+        User user = userService.findNowLoginUser();
+        List<BrandRes.LikedBrandList> likedBrandList= brandRepository.findAllByLikedUser(user).stream()
+                .map( origin -> {
+                    BrandRes.LikedBrandList likedBrand = BrandRes.LikedBrandList.builder()
+                            .likedBrandId(origin.getId())
+                            .categoryVId(origin.getCategoryV().getId())
+                            .categoryVName(origin.getCategoryV().getName())
+                            .name(origin.getName())
+                            .intro(origin.getIntro())
+                            .profileImg(origin.getProfileImg())
+                            .build();
+                    return likedBrand;
                 })
                 .collect(Collectors.toList());
+
+        return BrandRes.getLikedBrandRes.builder()
+                .count(likedBrandList.size())
+                .likedBrands(likedBrandList)
+                .build();
     }
 }
